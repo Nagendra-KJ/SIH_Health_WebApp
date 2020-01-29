eaf65e37f51aaf714e14b6e1dcd005c632ef27f2
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2020-01-29 06:14:32 +0530

    Rearranged patients html to health-workers

--- src/app/dashboard/health-workers/health-workers.component.ts
+++ src/app/dashboard/health-workers/health-workers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DBOperationsService } from 'src/app/shared/services/dboperations.service';
+import { Patient } from 'src/app/shared/models/patient';
+
 
 @Component({
   selector: 'app-health-workers',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthWorkersComponent implements OnInit {
 
-  constructor() { }
+  selectedPatient: Patient;
+  patients: Patient[];
+  loadedData: boolean = false;
+  constructor(private modalService: NgbModal, private dbOps: DBOperationsService) { }
 
   ngOnInit() {
-  }
+    var list = this.dbOps.getPatients();
+    var problem = list.then(data => {
+      data.forEach(element => {
+        this.patients = element;
+        this.patients.sort(function (a, b) {
+          var year1 = Number(a.appointmentDate.substring(6, 10));
+          var year2 = Number(b.appointmentDate.substring(6, 10));
+          if (year1 != year2)
+            return year2 - year1;
+          var month1 = Number(a.appointmentDate.substring(3, 5));
+          var month2 = Number(b.appointmentDate.substring(3, 5));
+          if (month1 != month2)
+            return month2 - month1;
+          var day1 = Number(a.appointmentDate.substring(0, 2));
+          var day2 = Number(b.appointmentDate.substring(0, 2));
+          return day2 - day1;
+        })
+      });
+      this.loadedData = true;
+    }
+    );
 
+    problem.catch(error => {
+      console.log("There was an error when reading the database " + error);
+    });
+  }
+  public viewPatientDetails(patient: any, id: string) {
+    this.selectedPatient = patient;
+    this.modalService.open(id);
+  }
+  public sendPhoneReminder() {
+    console.log("Reminder sent to " + this.selectedPatient.name + ". The phone number is " + this.selectedPatient.phoneNumber);
+  }
 }
--- src/app/dashboard/patients/patients.component.ts
+++ src/app/dashboard/patients/patients.component.ts
@@ -9,44 +9,8 @@ import { Patient } from 'src/app/shared/models/patient';
   styleUrls: ['./patients.component.css']
 })
 export class PatientsComponent implements OnInit {
-  selectedPatient: Patient;
-  patients: Patient[];
-  loadedData: boolean = false;
-  constructor(private modalService: NgbModal, private dbOps: DBOperationsService) { }
-
   ngOnInit() {
-    var list = this.dbOps.getPatients();
-    var problem = list.then(data => {
-      data.forEach(element => {
-        this.patients = element;
-        this.patients.sort(function (a, b) {
-          var year1 = Number(a.appointmentDate.substring(6, 10));
-          var year2 = Number(b.appointmentDate.substring(6, 10));
-          if (year1 != year2)
-            return year2 - year1;
-          var month1 = Number(a.appointmentDate.substring(3, 5));
-          var month2 = Number(b.appointmentDate.substring(3, 5));
-          if (month1 != month2)
-            return month2 - month1;
-          var day1 = Number(a.appointmentDate.substring(0, 2));
-          var day2 = Number(b.appointmentDate.substring(0, 2));
-          return day2 - day1;
-        })
-      });
-      this.loadedData = true;
-    }
-    );
 
-    problem.catch(error => {
-      console.log("There was an error when reading the database " + error);
-    });
-  }
-  public viewPatientDetails(patient: any, id: string) {
-    this.selectedPatient = patient;
-    this.modalService.open(id);
-  }
-  public sendPhoneReminder() {
-    console.log("Reminder sent to " + this.selectedPatient.name + ". The phone number is " + this.selectedPatient.phoneNumber);
   }
 
 }
